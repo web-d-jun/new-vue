68c4cc5bcfcb35013b450975732a070344920303
junyoung <cjy874545@gmail.com>
2020-06-05 01:09:12 +0900

    update

---
+++ src/components/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="listContainer">
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ListContainer"
+});
+</script>
+<style lang="scss" scoped>
+#listContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/layout/default/left.vue
+++ src/layout/default/left.vue
@@ -1,13 +1,9 @@
 <template>
-  <transition name="fade">
+  <transition name="slide-fade">
     <div id="leftContainer" v-if="$store.state.leftDrawer">
-      <!-- <div v-for="item in menuList" :key="item.name">{{item.name}}</div> -->
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      {{$store.state.leftDrawer}}
+      <List>
+        <div v-for="item in menuList" :key="item.name">{{item.name}}</div>
+      </List>
     </div>
   </transition>
 </template>
@@ -22,9 +18,13 @@ import {
 import Menu from "@/menu.ts";
 import { useStore } from "vuex";
 import { MutationTypes } from "../../store/mutation-types";
+import List from "@/components/list.vue";
 
 export default defineComponent({
   name: "leftContainer",
+  components: {
+    List
+  },
   setup() {
     const menuList = readonly<object>(Menu);
     const store = useStore();
@@ -52,6 +52,17 @@ export default defineComponent({
   flex: none;
   display: flex;
   flex-direction: column;
-  background-color: rgb(50,50,72);
+  background-color: rgb(50, 50, 72);
+}
+.slide-fade-enter-active {
+  transition: all 300ms ease;
+}
+.slide-fade-leave-active {
+  transition: all 800ms cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(-100px);
+  opacity: 0;
 }
 </style>
