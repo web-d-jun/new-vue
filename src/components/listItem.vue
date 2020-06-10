d8f585564d8f527a6d41595ec8ec88500dce903e
junyoung <cjy874545@gmail.com>
2020-06-11 01:40:45 +0900

    update

--- src/components/listItem.vue
+++ src/components/listItem.vue
@@ -1,21 +1,40 @@
 <template>
-  <div id="listItem">
+  <div id="listItem" class="material-ripple" @click="handleRippleShow">
+    <!-- <div class="animation" v-if="rippleShowState.value"></div> -->
     <slot />
   </div>
 </template>
 <script>
+import { ref, reactive, toRefs } from "vue";
 export default {
-  name: "ListItem"
+  name: "ListItem",
+  setup() {
+    const handleRippleShow = () => {
+      console.log('?')
+    };
+
+    return {
+      handleRippleShow
+    };
+  }
 };
 </script>
 <style lang="scss" scoped>
 #listItem {
+  position: relative;
   display: flex;
   flex-wrap: wrap;
   width: 100%;
   align-items: center;
   height: 50px;
   color: rgb(199, 199, 199);
+  .animation {
+    position: absolute;
+    width: 2px;
+    height: 2px;
+    border-radius: 50%;
+    background-color: #bdc3c7;
+  }
   &:hover {
     cursor: pointer;
     background-color: #43435e;
