ac7d7fc0de776e67550e1733540a7a4a6ac449d5
junyoung <cjy874545@gamil.com>
2020-10-28 21:38:55 +0900

    webpack 미사용

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
+// import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  rules: [
-    {
-      test: /\.s(c|a)ss$/,
-      use: [
-        "vue-style-loader",
-        "css-loader",
-        "sass-loader",
-        {
-          loader: "sass-loader",
-          // Requires sass-loader@^8.0.0
-          options: {
-            implementation: require("sass"),
-            sassOptions: {
-              fiber: require("fibers"),
-              indentedSyntax: true, // optional
-            },
-          },
-        },
-        {
-          loader: "sass-resources-loader",
-          options: {
-            resources: "./src/common.scss",
-          },
-        },
-      ],
-    },
-  ],
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      publicPath: "/",
-      title: "TEST1",
-      showErrors: true,
-    }),
-  ],
-};
+// module.exports = {
+//   rules: [
+//     {
+//       test: /\.s(c|a)ss$/,
+//       use: [
+//         "vue-style-loader",
+//         "css-loader",
+//         "sass-loader",
+//         {
+//           loader: "sass-loader",
+//           // Requires sass-loader@^8.0.0
+//           options: {
+//             implementation: require("sass"),
+//             sassOptions: {
+//               fiber: require("fibers"),
+//               indentedSyntax: true, // optional
+//             },
+//           },
+//         },
+//         {
+//           loader: "sass-resources-loader",
+//           options: {
+//             resources: "./src/common.scss",
+//           },
+//         },
+//       ],
+//     },
+//   ],
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       inject: false,
+//       hash: true,
+//       publicPath: "/",
+//       title: "TEST1",
+//       showErrors: true,
+//     }),
+//   ],
+// };
