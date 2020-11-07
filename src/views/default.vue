51e8feb95e39c8dcb3d28f347af24a0fc29a6887
junyoung <cjy874545@gamil.com>
2020-11-08 00:29:52 +0900

    update

--- src/views/default.vue
+++ src/views/default.vue
@@ -13,29 +13,27 @@
   </div>
 </template>
 <script lang="ts">
-import Header from "@/layout/default/header.vue";
-import Left from "@/layout/default/left.vue";
-import Right from "@/layout/default/right.vue";
-import { onMounted } from "vue";
-import { useStore } from "vuex";
-import { MutationTypes } from "../store/mutation-types";
-import Scrollbar from "smooth-scrollbar";
-import OverscrollPlugin from "smooth-scrollbar/plugins/overscroll";
+import Header from '@/layout/default/header.vue';
+import Left from '@/layout/default/left.vue';
+import Right from '@/layout/default/right.vue';
+import { onMounted } from 'vue';
+import { useStore } from 'vuex';
+import { MutationTypes } from '../store/mutation-types';
+import Scrollbar from 'smooth-scrollbar';
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 Scrollbar.use(OverscrollPlugin);
 
 export default {
-  name: "Default",
+  name: 'Default',
   components: {
     Header,
     Left,
-    Right
+    Right,
   },
   setup() {
     const store = useStore();
     onMounted(() => {
-      const scrollbar = Scrollbar.init(
-        document.querySelector<any>("#routerView")
-      );
+      const scrollbar = Scrollbar.init(document.querySelector('#routerView') as HTMLDivElement);
       scrollbar.addListener(status => {
         if (status.offset.y > 0) {
           store.commit(MutationTypes.SHOW_HEADER_SHADOW, true);
@@ -44,7 +42,7 @@ export default {
         }
       });
     });
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
