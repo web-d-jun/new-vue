071dca648f0d0fb7cf7aa9fb4d2d61568edb8cec
junyoung <cjy874545@gamil.com>
2020-11-09 00:20:24 +0900

    스터디 오른쪽 컨텐츠 추가

---
+++ src/layout/study/right.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="['right__container', { expand: $store.state.rightDrawer }]">
+    <div class="right__contents">
+      111111111111
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'right',
+});
+</script>
+<style lang="scss" scoped>
+.right__container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  transition: all 0.2s ease-in-out;
+  opacity: 0;
+  &.expand {
+    opacity: 1;
+  }
+  .right__contents {
+    width: 100%;
+    border: 1px solid;
+    height: 800px;
+    background: $darkBackground;
+    
+  }
+}
+</style>
