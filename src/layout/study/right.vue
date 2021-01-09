76ab81ddf1f75a1517e807828b3d8e2622168970
junyoung <cjy874545@gamil.com>
2021-01-09 23:01:13 +0900

    update

--- src/layout/study/right.vue
+++ src/layout/study/right.vue
@@ -1,34 +1,12 @@
 <template>
   <div :class="['right__container', { expand: $store.state.rightDrawer }]">
-    <div class="right__contents">
-      <div class="list__container">
+    <div id="rightContents" class="right__contents" >
+      <div id="listContainer" class="list__container" data-scrollbar>
         <div class="list__contents flex flex-wrap">
-          <div class="contents">
-            <div class="title">1. vue3</div>
-
+          <div class="contents" v-for="(item, index) in dataArray" :key="index">
+            <div class="title">{{ index + 1 }}.{{ item }}</div>
             <ul class="list">
               <li @click="$router.push('vue3/suspense')">1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-            </ul>
-          </div>
-          <div class="contents">
-            <div class="title">2. Typescript</div>
-
-            <ul class="list">
-              <li>1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-              <li>1.test</li>
-            </ul>
-          </div>
-          <div class="contents">
-            <div class="title">3. CSS</div>
-            <ul class="list">
-              <li>1.test</li>
               <li>1.test</li>
               <li>1.test</li>
               <li>1.test</li>
@@ -48,18 +26,78 @@
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
 import { useRouter } from '@/common/ts/function';
+import Scrollbar from 'smooth-scrollbar';
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
+Scrollbar.use(OverscrollPlugin);
 export default defineComponent({
   name: 'right',
   setup() {
     const router = useRouter();
-
+    const dataArray: Array<string> = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
     // router.push('/pass')
 
     const goPage = (url: string) => {
       console.log(url);
     };
+    onMounted(() => {
+      Scrollbar.init(document.querySelector('#listContainer') as HTMLDivElement);
+    });
     return {
       goPage,
+      dataArray,
     };
   },
 });
@@ -70,25 +108,37 @@ export default defineComponent({
   flex-direction: column;
   width: 0%;
   height: 100%;
-  transition: all 0.2s ease-in-out;
+  transition: all 0.4s ease-in-out;
   opacity: 0;
   &.expand {
     opacity: 1;
     width: 100%;
     .introduction {
+      color: $defaultColor !important;
+      transition: color 1s ease-in-out;
+      transition-delay: 2s;
       &--text1 {
         &::after {
-          animation: textWrite 3s steps(16) 1s 1 normal both;
+          animation: textWrite 3s steps(16) 3s 1 normal both;
+          background-color: #1e1f21 !important;
+          transition: background-color 0.2s ease-in-out;
+          transition-delay: 2s;
         }
       }
       &--text2 {
         &::after {
-          animation: textWrite 3s steps(14) 4s 1 normal both;
+          animation: textWrite 3s steps(14) 7s 1 normal both;
+          background-color: #1e1f21 !important;
+          transition: background-color 0.2s ease-in-out;
+          transition-delay: 2s;
         }
       }
       &--text3 {
         &::after {
-          animation: textWrite 3s steps(15) 7s 1 normal both;
+          animation: textWrite 3s steps(15) 10s 1 normal both;
+          background-color: #1e1f21 !important;
+          transition: background-color 0.2s ease-in-out;
+          transition-delay: 2s;
         }
       }
     }
@@ -97,12 +147,16 @@ export default defineComponent({
     width: 100%;
     border: 1px solid;
     height: 800px;
+    
     background: $darkBackground;
+    
     .list__container {
       width: 80%;
       height: 100%;
       padding-left: 30px;
       padding-top: 10px;
+      max-height: 600px;
+      overflow-y: auto;
       .list__contents {
         animation: listShow 2s;
         animation-delay: 2s;
@@ -117,6 +171,7 @@ export default defineComponent({
           .list {
             list-style: none;
             color: $defaultColor;
+            padding-inline-start: 20px;
           }
         }
       }
@@ -125,15 +180,16 @@ export default defineComponent({
       position: fixed;
       display: flex;
       flex-direction: row-reverse;
-      right: 7vh;
+      right: 6vh;
       top: 15vh;
       .introduction {
         position: relative;
-        color: $defaultColor;
+        color: transparent;
         width: 1px;
         word-break: break-word;
-        margin-left: 5vh;
-        line-height: 28px;
+        margin-left: 3vh;
+        line-height: 22px;
+        color: transparent;
         &::after {
           position: absolute;
           content: ' ';
@@ -142,7 +198,7 @@ export default defineComponent({
           left: 0;
           height: 100%;
           width: 23px;
-          background-color: #1e1f21;
+          background-color: transparent;
         }
       }
     }
