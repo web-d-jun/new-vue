22a99ff0134d245c8439ff4670769429ba1c3356
junyoung <cjy874545@gamil.com>
2021-01-17 16:30:19 +0900

    update

--- src/layout/study/right.vue
+++ src/layout/study/right.vue
@@ -1,7 +1,16 @@
 <template>
   <div :class="['right__container', { expand: $store.state.rightDrawer }]">
-    <div id="rightContents" class="right__contents" >
+    <div id="rightContents" class="right__contents">
       <div id="listContainer" class="list__container" data-scrollbar>
+        <div class="menu__container flex">
+          <div class="button__wrap"><button class="button">IAM</button></div>
+          <div class="button__wrap"><button class="button">Vue</button></div>
+          <div class="button__wrap"><button class="button">Typescript</button></div>
+          <div class="button__wrap"><button class="button">Javascript</button></div>
+          <div class="button__wrap"><button class="button">AWS</button></div>
+          <div class="button__wrap"><button class="button">CSS</button></div>
+        </div>
+        <JLines />
         <div class="list__contents flex flex-wrap">
           <div class="contents" v-for="(item, index) in dataArray" :key="index">
             <div class="title">{{ index + 1 }}.{{ item }}</div>
@@ -16,9 +25,9 @@
         </div>
       </div>
       <div class="introduction__container">
-        <div class="introduction introduction--text1">동짓달 기나긴 밤을 한 허리를 베어내어</div>
-        <div class="introduction introduction--text2">춘풍 이불 아래 서리서리 넣었다가</div>
-        <div class="introduction introduction--text3">님 오신 날 밤이어든 굽이굽이 펴리라</div>
+        <div class="introduction introduction--text1">ㄱㄴㄷ ㄹㅁㅂ ㅅㅇㅈ ㅊㅌㅍ ㅎ123</div>
+        <div class="introduction introduction--text2">45 67 89 ABCDEFG HIJKLMNO</div>
+        <!-- <div class="introduction introduction--text3">P QR S TUVW XYZ</div> -->
       </div>
     </div>
   </div>
@@ -28,9 +37,13 @@ import { defineComponent, onMounted } from 'vue';
 import { useRouter } from '@/common/ts/function';
 import Scrollbar from 'smooth-scrollbar';
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
+import JLines from '@/components/JLines.vue';
 Scrollbar.use(OverscrollPlugin);
 export default defineComponent({
   name: 'right',
+  components: {
+    JLines,
+  },
   setup() {
     const router = useRouter();
     const dataArray: Array<string> = [
@@ -147,9 +160,8 @@ export default defineComponent({
     width: 100%;
     border: 1px solid;
     height: 800px;
-    
     background: $darkBackground;
-    
+
     .list__container {
       width: 80%;
       height: 100%;
@@ -157,6 +169,42 @@ export default defineComponent({
       padding-top: 10px;
       max-height: 600px;
       overflow-y: auto;
+      .menu__container {
+        width: 100%;
+        height: 50px;
+        padding: 8px 0;
+        .button__wrap {
+          padding: 0 8px;
+          align-items: center;
+          .button {
+            position: relative;
+            width: auto;
+            min-width: 100px;
+            height: 100%;
+            border: 1px solid transparent;
+            outline: 0;
+            background: rgb(0, 0, 0);
+            border-radius: 4px;
+            color: $defaultColor;
+            @include cursor(pointer);
+            font-size: 17px;
+            &::after {
+              --border-width: 3px;
+              content: '';
+              position: absolute;
+              left: calc(-1 * var(--border-width));
+              top: calc(-1 * var(--border-width));
+              width: calc(100% + var(--border-width) * 2);
+              height: calc(100% + var(--border-width) * 2);
+              background-size: 200% 200%;
+              background: linear-gradient(45deg, #d5007d, #e53935);
+              z-index: -1;
+            }
+            &:hover {
+            }
+          }
+        }
+      }
       .list__contents {
         animation: listShow 2s;
         animation-delay: 2s;
@@ -172,6 +220,7 @@ export default defineComponent({
             list-style: none;
             color: $defaultColor;
             padding-inline-start: 20px;
+            @include cursor(pointer);
           }
         }
       }
